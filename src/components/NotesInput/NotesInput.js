5073634f32af4d8a20db0d42046a394f3a549725
Robertus Adrian <robertusadrian@gmail.com>
2022-08-08 17:03:36 +0700

    Update noteinput.js

--- src/components/NotesInput/NotesInput.js
+++ src/components/NotesInput/NotesInput.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function NotesInput() {
+export default function NotesInput(props) {
+  const [listItem, setListItem] = useState({
+    id: +0,
+    title: "",
+    body: "",
+    createdAt: new Date(),
+    archived: false,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addNotes(listItem);
+  };
+
   return (
     <div className="note-input">
       <h2>Buat Catatan</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <p className="note-input__title__char-limit"></p>
         <input
           type="text"
           className="note-input__title"
           placeholder="ini adalah judul ..."
+          onChange={(e) => {
+            setListItem({
+              ...listItem,
+              title: e.target.value,
+            });
+          }}
+          value={listItem.title}
           required
         />
         <textarea
-          name=""
-          id=""
+          name="body"
+          id="body"
           cols="30"
           rows="10"
           className="note-input__body"
           placeholder="tulis catatanmu disini..."
+          onChange={(e) => {
+            setListItem({
+              ...listItem,
+              body: e.target.value,
+            });
+          }}
+          value={listItem.body}
         ></textarea>
         <button type="submit">Buat</button>
       </form>

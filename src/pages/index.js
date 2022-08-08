af8cbfcb2cdd4442ddab34f0f49cbcac8b132d7c
Robertus Adrian <robertusadrian@gmail.com>
2022-08-08 17:03:02 +0700

    Update Page index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import ArchiveSection from "../components/ArchiveSection";
 import HeaderSection from "../components/HeaderSection";
 import NotesInput from "../components/NotesInput/NotesInput";
 import NotesApp from "../components/NotesSection/NotesApp";
-export default class LandingPage extends Component {
-  componentDidMount() {
-    document.title = "My Personal Notes.";
-    window.scrollTo(0, 0);
-  }
-  render() {
-    return (
-      <>
-        {/* <HeaderSection /> */}
-        <HeaderSection />
-        <div className="note-app__body">
-          <NotesInput />
-          <NotesApp />
-          <ArchiveSection />
-        </div>
-      </>
-    );
-  }
+export default function LandingPage() {
+  const addNotesHandler = ({ value }) => {
+    console.log(value);
+  };
+
+  return (
+    <>
+      {/* <HeaderSection /> */}
+      <HeaderSection />
+      <div className="note-app__body">
+        <NotesInput />
+        <NotesApp />
+        <ArchiveSection />
+      </div>
+    </>
+  );
 }

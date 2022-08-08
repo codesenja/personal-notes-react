ce729aeafc9f075009b855de7f42aada3fcc633d
Robertus Adrian <robertusadrian@gmail.com>
2022-08-08 17:04:10 +0700

    Update NotesApp.js

--- src/components/NotesSection/NotesApp.js
+++ src/components/NotesSection/NotesApp.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { getInitialData, showFormattedDate } from "../../utils";
 import NotesList from "./NotesList";
 
 export default function NotesApp() {
-  const notes = getInitialData();
+  const [notes, setNotes] = useState(getInitialData());
+  const onDeleteHandler = (id) => {
+    const item = notes.filter((note) => note.id !== id);
+    setNotes(item);
+  };
+
   return (
     <>
       <h2>Catatan Aktif</h2>
-      <NotesList notes={notes} />
+      <NotesList notes={notes} onDelete={onDeleteHandler} />
     </>
   );
 }

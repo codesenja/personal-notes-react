7be835fa50096f7b4515c2d2aeb60636b3d1267c
robertusAdrian <robertusadrian@gmail.com>
2022-09-08 14:07:59 +0700

    fixed bug router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../src/pages";
 import AddPage from "./pages/add";
 import DetailPage from "./pages/detail";
@@ -12,10 +12,12 @@ import "./styles/style.css";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/add" element={<AddPage />} />
-    <Route path="/detail/:id" element={<DetailPage />} />
-    <Route path="/*" element={<Fouronfour />} />
-    {/* <Route /> */}
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/add" element={<AddPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route path="/*" element={<Fouronfour />} />
+      {/* <Route /> */}
+    </Routes>
   </BrowserRouter>
 );
